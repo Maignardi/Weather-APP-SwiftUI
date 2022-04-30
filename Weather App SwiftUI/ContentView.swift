9e5d96ac4c5a50280192c5be067a43cdc0d4f833
mmaignardi <matheus.maignardi@meliuz.com.br>
2022-04-30 15:02:40 -0400

    Initial Commit

---
+++ Weather App SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather App SwiftUI
+//
+//  Created by Matheus Mendonça Maignardi on 30/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather App SwiftUI/Weather_App_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_App_SwiftUIApp.swift
+//  Weather App SwiftUI
+//
+//  Created by Matheus Mendonça Maignardi on 30/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Weather_App_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
